7dd277fc3e47ea2a27c00af2387505413c557254
Simon Archer <simon.james.archer@gmail.com>
2021-09-25 22:46:00 +0100

    listing functions to interact with cans state

--- src/store/cans-slice.js
+++ src/store/cans-slice.js
@@ -1,9 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cansSlice = createSlice({
-  name: "cans",
-  initialState = {},
-  reducers: {},
+    name: "cans",
+    initialState = {
+        items: []
+    },
+    reducers: {
+        addItem(state, action) {
+            // Add logic
+        },
+        deleteItem(state, action) {
+            // Add logic
+        },
+        increaseItem(state, action) {
+            // add logic
+        },
+        decreaseItem(state, action) {
+            // add logic
+        }
+
+    },
 });
 
 export const cansActions = cansSlice.actions;
