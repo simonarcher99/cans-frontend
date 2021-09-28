3a635873a98c97e4ad28e3eaf12c4cae4a7e6afb
Simon Archer <simon.james.archer@gmail.com>
2021-09-28 17:06:32 +0100

    Adding search bar component

---
+++ src/components/UI/SearchBar.js
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+  return (
+    <section>
+      <form>
+        <input placeholder="Search"></input>
+        <button></button>
+      </form>
+    </section>
+  );
+};
+
+export default SearchBar;
