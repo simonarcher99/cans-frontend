83fb304b684fc0420e352b2fd598583a53355e6f
Simon Archer <simon.james.archer@gmail.com>
2021-10-23 11:14:31 +0100

    Linking login form up to token endpoint on backend

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,30 +1,80 @@
 import React from "react";
 
+import useInput from "../hooks/use-input";
+
 import { useDispatch, useSelector } from "react-redux";
+import Button from "../components/UI/Button";
 import { authActions } from "../store/auth-slice";
 
 import { TOKEN_URL } from "../utilities/constants";
 
 const AuthPage = () => {
-  const email = "test@test.com";
-  const password = "password";
+  const dispatch = useDispatch();
 
-  fetch(TOKEN_URL, {
-    method: "POST",
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.token);
-    });
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    inputBlurHandler: emailBlurHandler,
+    valueChangeHandler: emailChangeHandler,
+    reset: emailReset,
+  } = useInput((value) => value.trim() !== "");
 
-  return <h1>This is the login page</h1>;
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    inputBlurHandler: passwordBlurHandler,
+    valueChangeHandler: passwordChangeHandler,
+    reset: passwordReset,
+  } = useInput((value) => value.trim() !== "");
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    fetch(TOKEN_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.token);
+        dispatch(authActions.login({ token: data.token }));
+      })
+      .then((data) => {
+        emailReset();
+        passwordReset();
+      });
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <div>
+        <label>Email</label>
+        <input
+          value={email}
+          onBlur={emailBlurHandler}
+          onChange={emailChangeHandler}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          value={password}
+          onBlur={passwordBlurHandler}
+          onChange={passwordChangeHandler}
+        />
+      </div>
+      <button type="submit" onClick={onSubmitHandler}>
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default AuthPage;
